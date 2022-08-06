7f5397da1c982d7f3da91392189581bfde6c1595
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2022-08-06 15:08:08 -0400

    ajout initial

---
+++ index.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'meta.php'; ?>
+
+    <title>Maison Eiffel Home</title>
+</head>
+<body>
+   <div class="container">
+   <div class="header">
+        <div class="header__top">
+
+        </div>
+
+        <div class="header__main">
+
+        </div>
+    </div>
+
+    <div class="hero">
+        <div class="hero__links">
+            <a href="l">
+                Epilation laser
+            </a>
+
+            <a href="l">
+                Epilation laser
+            </a>
+
+            <a href="l">
+                Epilation laser
+            </a>
+        </div>
+
+        <div class="hero__main">
+            <h1>
+                Lorem ipsum
+            </h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, 
+                ut quidem repellat repudiandae fugiat optio natus debitis est minima ullam, 
+                aliquid iure molestiae? Suscipit rem pariatur est qui blanditiis sint.
+            </p>
+
+            <div class="line">
+                <hr>
+                <a href="">En savoir plus</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="about">
+        <div class="about__infos">
+            <h2 class="about__infos__title">
+                Notre institut en quelques 
+            </h2>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Accusantium, repellat, porro doloremque fugit odio eius blanditiis saepe impedit
+                 facilis laborum aut laudantium ipsam magnam vel atque obcaecati veniam ex. Optio.
+            </p>
+
+            <div class="about__infos__numbers">
+                <div class="number">
+                    <div class="span">
+                        +99
+                    </div> <br>
+                    lorem
+                </div>
+
+                <div class="number">
+                    <div class="span">
+                        +99
+                    </div> <br>
+                    lorem
+                </div>
+
+                <div class="number">
+                    <div class="span">
+                        +99
+                    </div> <br>
+                    lorem
+                </div>
+            </div>
+        </div>
+
+        <div class="about__image">
+            <img src="" alt="">
+        </div>
+    </div>
+
+    <div class="specialities">
+        <h2 class='title'>
+            NOS SPECIALITES
+        </h2>
+
+        <p class="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            A molestiae animi ad officia accusamus debitis voluptatem vel reprehenderit,
+             temporibus commodi ab, maiores suscipit fugiat veniam veritatis facere nam, nulla laudantium?
+        </p>
+        
+        <div class="specialities__list">
+                <div class="speciality">
+                    <img src="" alt="">
+                    <h3>
+                        EPILATION LASER
+                    </h3>
+                </div>
+
+                <div class="speciality">
+                    <img src="" alt="">
+                    <h3>
+                        EPILATION LASER
+                    </h3>
+                </div>
+
+                <div class="speciality">
+                    <img src="" alt="">
+                    <h3>
+                        EPILATION LASER
+                    </h3>
+                </div>
+        </div>
+
+    </div>
+
+    <div class="center">
+        <h2 class="title">
+            NOTRE CENTRE ET NOS EQUIPES
+        </h2>
+
+        <P class="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Quod magnam molestias officiis placeat maiores 
+            quaerat cupiditate labore aliquam? Illo aliquam voluptates 
+            placeat tempora dolorum ad iure fuga dolore excepturi natus?
+        </P>
+
+        <div class="Slider">
+            <div class="slider__images">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                
+            </div>
+
+            <div class="slider__controls">
+
+            </div>
+        </div>
+
+
+    </div>
+
+    <div class="testimonies">
+        <h2 class="title">
+            LES TEMOIGNAGES
+        </h2>
+
+        <p>
+            lorem
+        </p>
+
+        <div class="testimonies__list">
+            <div class="testimony">
+                    <div class="testimony__top">
+                        <img src="" alt="">
+                        <span>author</span>
+                        <div class="stars">
+                            ****
+                        </div>
+                    </div>
+            </div>
+
+            <div class="testimony">
+                    <div class="testimony__top">
+                        <img src="" alt="">
+                        <span>author</span>
+                        <div class="stars">
+                            ****
+                        </div>
+                    </div>
+            </div>
+
+            <div class="testimony">
+                    <div class="testimony__top">
+                        <img src="" alt="">
+                        <span>author</span>
+                        <div class="stars">
+                            ****
+                        </div>
+                    </div>
+            </div>
+
+            <div class="testimony">
+                    <div class="testimony__top">
+                        <img src="" alt="">
+                        <span>author</span>
+                        <div class="stars">
+                            ****
+                        </div>
+                    </div>
+            </div> 
+        </div>
+    </div>
+
+    <div class="blog">
+        <h2 class="title">
+            NOTRE BLOG
+        </h2>
+
+        <P>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellat, assumenda, omnis perspiciatis, maxime voluptates sequi deserunt 
+            inventore voluptate illo exercitationem cum ducimus architecto. 
+            Eum ipsam, incidunt dolorem ducimus animi reiciendis!
+        </P>
+
+        <div class="cards_">
+            <div class="cards__card">
+                    <img src="" alt="">
+                    <div class="card__info">
+                        <h4>
+                            lorem
+                        </h4>
+
+                        <p class="text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Minus officiis enim architecto, sapiente eveniet nemo commodi
+                        </p>
+
+                        <div class="time">
+                            <i></i>
+                        4 mois
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+       <div class="link">
+        <a href="blog.php">
+            Lire plus d'articles
+        </a>
+       </div>
+    </div>
+
+    <div class="footer">
+        <div class="footer__log">
+            <img src="" alt="">
+        </div>
+
+        <div class="footer__links">
+            <a href="\"></a>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+
+        <div class="footer__infos">
+            <div class="info">
+                Paris 11 <br>
+                <i></i>0136677br !
+                <a href="">
+                    Prendre rdv
+                </a>
+            </div>
+
+            <div class="info">
+                Paris 11 <br>
+                <i></i>0136677br !
+                <a href="">
+                    Prendre rdv
+                </a>
+            </div>
+        </div>
+    </div>
+
+   </div>
+</body>
+</html>
---
+++ specialites.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'meta.php'; ?>
+    
+    <title>Nos specialites</title>
+</head>
+<body>
+    <div class="container">
+        <?php include 'header.php;' ?>
+
+        <div class="speciality-hero">
+            <h1>
+                Soins visages
+            </h1>
+
+            <p class="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Earum labore vitae deserunt nam assumenda sint sed repellendus! Eaque 
+                consequuntur aut error dicta! Vitae, rerum. Dignissimos possimus illum in doloribus sint.
+            </p>
+
+            <div class="link">
+                <a href="">
+                    En savoir plus
+                </a>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="content__text">
+                <h2 class="title">
+                    Des traitements adaptes 
+                </h2>
+
+                <p class="text">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Mollitia, doloribus? Tenetur totam laboriosam cupiditate accusantium nam, maiores, animi 
+                    ratione iusto dolore illo dolor nihil ad ipsum aspernatur illum exercitationem quos!
+                </p>
+            </div>
+
+            <div class="content__image">
+                <div class="image">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="content__image">
+                <img src="" alt="">
+            </div>
+
+            <div class="content__text">
+                <h2 class="title">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                </h2>
+
+               <p class="text">
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Voluptatem itaque porro sed vitae illum voluptas fuga ea. 
+                Omnis asperiores saepe dolore id sunt nobis ea placeat, nam voluptatem blanditiis deserunt!
+
+               </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
