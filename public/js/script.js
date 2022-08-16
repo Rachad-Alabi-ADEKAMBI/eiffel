8ab2f41cccd3c13cdef0bbe8a57fe4b038cc610d
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2022-08-16 22:03:33 +0100

    update files

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+function displayMenu() {
+    document.getElementById("header__list").style.display = "block";
+  //  document.getElementById("mobile-menu-btn").style.display = "none";
+    //console.log('ok');
+}
+
+function closeMenu() {
+    document.getElementById("header__list").style.display = "none";
+//    document.getElementById("mobile-menu-btn-close").style.display = "none";
+    //console.log('ok');
+}
