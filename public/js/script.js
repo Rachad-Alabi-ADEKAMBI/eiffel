ba899051c765300d00ed896720327da20fb4ec55
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2022-08-20 21:31:54 +0100

    update files

--- public/js/script.js
+++ public/js/script.js
@@ -9,5 +9,36 @@ function closeMenu() {
     document.getElementById("header__list").style.display = "none";
     document.getElementById("close").style.display = "none";
     document.getElementById("open").style.display = "block";
-
 }
+
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
